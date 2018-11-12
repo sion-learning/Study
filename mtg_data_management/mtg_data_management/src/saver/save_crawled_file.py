9ca97639c612157b65ebce4cd52c4ea0feb66e17
sion-learning <yu3.tech@gmail.com>
2018-11-13 07:32:46 +0900

    ディレクトリ構成を変更

--- mtg_data_management/mtg_data_management/src/saver/save_crawled_file.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-
-
-def extract_html_to_file(html, path):
-    with open(path, mode='wb') as f:
-        f.write(html.encode('CP932'))
-
-    return None
--- mtg_data_management/mtg_data_management/src/tester/crawl_test.py
+++
@@ -1,7 +0,0 @@
-# encoding; utf-8
-
-from src.crawler import get_html as gh
-
-html = gh.get_html_soup("http://www.hareruyamtg.com/jp/c/cGRN-R/")
-
-print(html)
